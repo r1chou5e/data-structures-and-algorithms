bb17a15fc43de15e72d5bdf76fb503e4179c872e
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2024-02-02 16:55:14 +0700

    [add] generate-subarray

--- algorithms/dynamic-programming/conference-scheduling/index.js
+++ algorithms/dynamic-programming/conference-scheduling/index.js
@@ -2,3 +2,11 @@
 // Cho một danh sách các cuộc họp với thời gian bắt đầu và kết thúc, tìm số lượng lớn nhất các cuộc họp không trùng nhau có thể tổ chức trong một phòng.
 // Input 1: [[2, 3], [4, 6], [2, 5], [5, 12]] -> Output 1: 2
 // Input 2: [[8, 10], [8.5, 11], [7, 9], [10, 12], [13, 17], [12.5, 14]] -> Output 2: 3
+const maxMeetingScheduling = (meetings) => {};
+
+const isConflicted = (meetingA, meetingB) => {
+  if (meetingA[0] >= meetingB[1] || meetingB[0] >= meetingA[1]) return false;
+  return meetingA[1] > meetingB[0] || meetingA[0] < meetingB[1];
+};
+
+console.log(isConflicted([14, 19], [12, 13]));
---
+++ data-structure/array/generate-subarray/index.js
@@ -0,0 +1,19 @@
+// Bài toán sinh tất cả mảng con của mảng A
+// Input 1: [1, 3, 5, 10] -> Output 1: [[1],[3],[5],[10], [1,3], [3,5], [5,10], [1,5], [3,10], [1,10], [1,3,5], [3,5,10], [1,3,10], [1,5,10], [1,3,5,10], []]
+
+const generateSubarrays = (arr) => {
+  const result = [];
+
+  const combine = (current, start) => {
+    for (let i = start; i < arr.length; i++) {
+      result.push([...current, arr[i]]);
+      combine([...current, arr[i]], i + 1);
+    }
+  };
+
+  combine([], 0);
+
+  return result;
+};
+
+console.log(generateSubarrays([1, 9, 3, 10]));
